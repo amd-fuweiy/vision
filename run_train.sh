7e36098e291725f7c74a17f9987a4614edc71a5a
amd-fuweiy <fuweiy@amd.com>
2025-06-09 04:59:10 +0000

    enable torch compile and NHWC layout

---
+++ run_train.sh
@@ -0,0 +1,23 @@
+unset MIOPEN_FIND_MODE
+unset MIOPEN_FIND_ENFORCE
+
+COMPILE_AGRS="--compile"    #COMPILE_ARGS=""
+
+USE_NHWC_LAYOUT=true
+if [ "$USE_NHWC_LAYOUT" == "true" ]; then
+    export PYTORCH_MIOPEN_SUGGEST_NHWC=1
+    export PYTORCH_MIOPEN_SUGGEST_NHWC_BATCHNORM=1
+    LAYOUT_ARGS="--channel-last"
+    echo "Training with NHWC layout"
+else
+    export PYTORCH_MIOPEN_SUGGEST_NHWC=0
+    export PYTORCH_MIOPEN_SUGGEST_NHWC_BATCHNORM=0
+    LAYOUT_ARGS=""
+    echo "Training with NCHW layout"
+fi
+
+DATAPATH=/mnt/nvme0/data/datasets/imagenet
+MODEL=resnet50
+GBS=128
+torchrun --nproc_per_node=8 references/classification/train.py --data-path $DATAPATH \
+        --model $MODEL --epochs 300 --batch-size $GBS --opt adamw --lr 0.001 --weight-decay 0.05 --norm-weight-decay 0.0  --bias-weight-decay 0.0 --transformer-embedding-decay 0.0 --lr-scheduler cosineannealinglr --lr-min 0.00001 --lr-warmup-method linear  --lr-warmup-epochs 20 --lr-warmup-decay 0.01 --amp --label-smoothing 0.1 --mixup-alpha 0.8 --clip-grad-norm 5.0 --cutmix-alpha 1.0 --random-erase 0.25 --interpolation bicubic --auto-augment ta_wide --model-ema --ra-sampler --ra-reps 4  --val-resize-size 224 $LAYOUT_ARGS $COMPILE_ARGS
